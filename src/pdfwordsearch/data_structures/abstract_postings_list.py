8ff4ab4f40b0b6151b4786f540d571faae61f841
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-04-06 14:58:56 -0400

    quick fix

--- src/pdfwordsearch/data_structures/abstract_postings_list.py
+++ src/pdfwordsearch/data_structures/abstract_postings_list.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractclassmethod, abstractmethod
 from typing import Iterator, Tuple
 
-from pdfwordsearch.data_structures.postings_list import PostingsList
-
 class AbstractPostingsList(ABC):
     @abstractmethod
     def add_word(self, word: str, word_count: int, docid: int) -> None:
--- tests/unit_tests/test_postings_list.py
+++ tests/unit_tests/test_postings_list.py
@@ -3,9 +3,9 @@ from pdfwordsearch.data_structures.postings_list import PostingsList
 
 def test_add_word_postings_list():
     postings_list = PostingsList()
-    postings_list.add_word("happy", 0)
+    postings_list.add_word("happy", 0, 1)
 
     actual_result = postings_list.get_locations("happy")
-    expected_result = [0]
+    expected_result = [(0,1)]
 
     assert actual_result == expected_result
