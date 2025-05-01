842e68482467479695e7ee73ec1974fa8854d8f0
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-05-01 11:09:10 -0400

    init coordinators

--- src/pdfwordsearch/pdfwordsearch.py
+++ src/pdfwordsearch/pdfwordsearch.py
@@ -1,28 +1,10 @@
 import argparse
 
+from pdfwordsearch.coordinators.io_coordinator import IOCoordinator
+
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog="pdfwordsearch",
-        description="smart pdf querying",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-
-    parser.add_argument("pdf", help="pdf path")
-    parser.add_argument("query", help="search query")
-    parser.add_argument(
-        "-c", "--count", type=int, default=10, help="max number of matches to output"
-    )
-    parser.add_argument(
-        "-i", "--image", action="store_true", help="search images for text"
-    )
-    parser.add_argument(
-        "-r", "--range", type=str, default="all", help="specify page range"
-    )
-    parser.add_argument(
-        "-s", "--sensitive", action="store_true", help="case insensitive search"
-    )
-    args = parser.parse_args()
+    IOCoordinator.run()
 
 
 if __name__ == "__main__":
