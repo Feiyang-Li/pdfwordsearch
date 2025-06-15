718252e80880801c89af86fd8090c0e9086d285f
Feiyang <feiyang.f.li@gmail.com>
2025-06-15 12:10:32 -0400

    changed main.py in the backend not a big change

--- src/backend/main.py
+++ src/backend/main.py
@@ -21,12 +21,12 @@ apl_store : Dict[Cookie, CompressedPostingsList] = dict()
 
 
 @app.post("/pdf_to_apl/")
-async def pdfToApl(response: Response, file: UploadFile = File(...), page_ignore: Optional[List[int]] = Form(None), encode: str = "utf8", save: str = None):
+async def pdfToApl(file: UploadFile = File(...), page_ignore: Optional[List[int]] = Form(None), encode: str = "utf8", save: str = None):
     """
     Converts the file to an APL and returns a cookie
     Parameters
     ----------
-    response :
+    response :d
     file :
     page_ignore :
     encode :
@@ -41,21 +41,23 @@ async def pdfToApl(response: Response, file: UploadFile = File(...), page_ignore
     if file.content_type != "application/pdf":
         return JSONResponse(status_code=400, content={"error": "Only PDF files are allowed."})
 
+    file_bytes = await file.read()
     ## process 1: obtaining the abstract table:
-    fl = pdf_info_get(file.file, ignore_page=page_ignore, encode=encode, save=save, is_binary=True)
+    fl = pdf_info_get(file_bytes, ignore_page=page_ignore, encode=encode, save=save, is_binary=True)
     ## process 2: convert to compressed posting list
     apl = pdf_to_pl(fl, CompressedPostingsList)
 
     # Associate APL with cookie
     unique_key = str(uuid.uuid4())
 
+    response = JSONResponse(content={"Process": "pdf to apl process completed"})
     response.set_cookie(key=unique_key)
     apl_store[unique_key] = cast(apl, CompressedPostingsList)
 
-    return {"Process": "pdf to apl process completed"}
+    return {"response": response, "sid": unique_key} 
 
 @app.get("/apl/command/{query}")
-async def query_apl(query: str, cookie : Annotated[str | None, Cookie()] = None, CI_syn : float = 0.1, CI_sim : float = 0.7):
+async def query_apl(query: str, session_id: Annotated[str | None, Cookie()] = None, CI_syn : float = 0.1, CI_sim : float = 0.7):
     """
     Execute a query on the APL. Uses cookies to differentiate between users
     Parameters
@@ -69,8 +71,8 @@ async def query_apl(query: str, cookie : Annotated[str | None, Cookie()] = None,
     -------
 
     """
-    if not cookie in apl_store:
+    if not session_id in apl_store:
         raise HTTPException(status_code=403, detail="Invalid cookie.")
 
-    results = execute_query(query=query, postings_list=apl_store[cookie])
+    results = execute_query(query=query, postings_list=apl_store[session_id])
     return {results}
