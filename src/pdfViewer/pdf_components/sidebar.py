f3847e39f16feffc3965cda55f634a9d777efc39
blaicheo <78671393+blacheo@users.noreply.github.com>
2025-06-29 21:23:34 -0400

    added sidebar

--- src/pdfViewer/pdf_components/sidebar.py
+++ src/pdfViewer/pdf_components/sidebar.py
@@ -1,13 +1,16 @@
 import tkinter as tk
+from pathlib import Path
+from threading import Thread
 from typing import Callable, Optional
 from ttkbootstrap import Entry, Button, Frame
 
 from pdfViewer.pdf_components.results import Results
 from pdfwordsearch.data_structures.abstract_postings_list import AbstractPostingsList
-from pdfwordsearch.data_structures.compressed_postings_list import CompressedPostingsList
+from pdfwordsearch.data_structures.compressed_postings_list import (
+    CompressedPostingsList,
+)
+from pdfwordsearch.scan.pdf_scan import pdf_info_get
 
-
-MAX_RESULTS_DISPLAY = 5
 
 class Sidebar(Frame):
     def __init__(self, master, display_page_function: Callable[[int], None]):
@@ -16,31 +19,57 @@ class Sidebar(Frame):
         self.pl: Optional[AbstractPostingsList] = None
         self.display_page_function = display_page_function
 
+        self.search_widget = Frame(self)
         # Create an entry widget for user input
-        self.entry = Entry(master)
+        self.entry = Entry(self.search_widget)
         self.entry.pack(side=tk.LEFT, fill=tk.X, anchor=tk.NW)
 
         # Create a search button
-        self.search_button = Button(master, text="Search")
+        self.search_button = Button(
+            self.search_widget, text="Search", command=self._perform_search
+        )
         self.search_button.pack(side=tk.LEFT, anchor=tk.NW)
+        self.search_widget.pack(side=tk.TOP, anchor=tk.NW)
 
-        self.results = Results(master)
-        self.results.pack(side=tk.LEFT, anchor=tk.SW)
+        self.results = Results(self, self.display_page_function)
+        self.results.pack(side=tk.TOP, anchor=tk.SW)
 
+        self.pdf_info = None
 
-    def load_pdf_file(self):
-        self.pl = CompressedPostingsList()
+    def load_pdf_file(
+        self, file_path: Optional[str] = None, file: Optional[bytes] = None
+    ):
+        if file_path is None and file is None:
+            raise ValueError("Either file_path or file must be provided")
 
-    def perform_search(self):
+        if file_path is not None:
+            info = pdf_info_get(file_path)
+        else:
+            info = pdf_info_get(file, is_binary=True)
+
+        self.pdf_info = info
+        self.pl = CompressedPostingsList(info)
+        self.results.update_file(info)
+
+    def _perform_search(self):
         results = self.pl.execute_query(self.entry.get())
+        self.results.update_results(results)
 
-        i = 0
-        for result in results:
-            i += 1
-            if i > MAX_RESULTS_DISPLAY:
-                break
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = tk.Tk()
-    sidebar = Sidebar(root, lambda _: None)
+    current_dir = Path(__file__).parent
+    sidebar = Sidebar(root, lambda a: print(f"Go to page {a}"))
+    sidebar.pack(side=tk.LEFT)
+
+    t1 = Thread(
+        target=sidebar.load_pdf_file(
+            str(
+                current_dir.joinpath(
+                    "../../../tests/resources/List_of_chiropterans.pdf"
+                )
+            )
+        )
+    )
+    t1.start()
     root.mainloop()
