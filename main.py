4436e0e7920134cca674ef67dcd323e36ff17853
Feiyang <feiyang.f.li@gmail.com>
2025-07-27 08:42:10 -0400

    added wordnet package into the resource and python file for import

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.pdf_viewer.main import *
+from resource.nltk_data.corpora.importWordnet import *
+
 
 if __name__ == "__main__":
     root = tk.Tk()
---
+++ resource/nltk_data/corpora/importWordnet.py
@@ -0,0 +1,2 @@
+import nltk
+nltk.data.path.append('pdfwordsearch/resource/nltk_data')
